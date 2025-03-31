61038970c1ff0cea0c2d11306fd894609a8ca4c4
rakesh875u <devmalya1.pramanik@stu.adamasuniversity.ac.in>
2025-04-01 00:01:27 +0530

    Add files via upload

---
+++ myportfolio.js
@@ -0,0 +1,7 @@
+document.getElementById("contactForm").addEventListener("submit", function(event) {
+    event.preventDefault();
+    alert("Message Sent! We will get back to you soon.");
+    document.getElementById("contactForm").reset();
+});
+//Devmalya Pramanik.
+//roll:-142
